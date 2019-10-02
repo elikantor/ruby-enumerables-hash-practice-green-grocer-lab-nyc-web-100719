feed94b5702bbe38df9b5ceb6b1b2bdb05fc4d3d
Eli Kantor <eli.kantor@outlook.com>
2019-10-02 15:10:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,7 @@ def checkout(items, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
 
-  total = applied_discount.reduce(0) { |acc, (key, value)| binding.pry}
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
   
 
 end
